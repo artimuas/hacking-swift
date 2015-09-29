16c8403031fdb26c6bf8b897aa8ee078b612deee
Saumitra Vaidya <saumitra@Saumitras-MacBook-Pro.local>
2015-09-28 22:51:09 -0500

    Project-4 - Initial Commit

---
+++ SimpleBrowser/SimpleBrowser/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by Saumitra Vaidya on 9/20/15.
+//  Copyright © 2015 agratas. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
