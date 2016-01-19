ec4f3244f62ddfe8dbed4760456ff06bd6156ee4
Saumitra Vaidya <saumitra@s21mi.local>
2016-01-18 21:57:17 -0600

    WIP

---
+++ Petitions/Petitions/MasterViewController.swift
@@ -0,0 +1,97 @@
+//
+//  MasterViewController.swift
+//  Petitions
+//
+//  Created by Saumitra Vaidya on 1/18/16.
+//  Copyright © 2016 agratas. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [[String: String]]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetcData()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object.description
+        return cell
+    }
+    
+    //MARK: - Fetch 
+    
+    func fetcData() {
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = NSURL(string: urlString) {
+            if let data = try? NSData(contentsOfURL: url, options: []) {
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    //parse
+                    parseJSON(json)
+                }
+            }
+        }
+    }
+    
+    func parseJSON(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
+
+}
+
